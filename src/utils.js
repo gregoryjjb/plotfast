545f38187d4bb01b73132b391a16f7242297c6d7
Greg Brisebois <gregoryjjb@gmail.com>
2018-10-24 00:51:43 -0700

    Added log10 polyfill for IE support

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,13 @@
+// Log10 Polyfill
+// Courtesy of MDN
+Math.log10 = Math.log10 || function (x) {
+	return Math.log(x) * Math.LOG10E;
+};
 
+/**
+ * Get the number of digits from a number
+ * @param {number} x Number to examine
+ */
 export const numDigits = x => {
 	return Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1;
 }
@@ -9,7 +18,7 @@ export const numDigits = x => {
  * @param {*} n Digits to round to
  */
 export const round = (x, n) => {
-	if(typeof n != 'number')
+	if (typeof n != 'number')
 		n = numDigits(x) - 1;
 	let roundTo = Math.pow(10, n);
 	return Math.round(x / roundTo) * roundTo;
