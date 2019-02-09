4007221e474efcc1413e6760a01eafd16ef13027
Greg Brisebois <gregory.brisebois@analog.com>
2019-02-08 16:40:13 -0800

    Fixed error to not throw every publish

--- scripts/throw-error.js
+++ scripts/throw-error.js
@@ -1,1 +1,18 @@
-throw new Error('Use `yarn run release`, not `npm publish` or `yarn publish`');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('DID YOU RUN `release` NOT `publish`??? (Y/N): ', answer => {
+    if (answer === 'Y' || answer === 'y') {
+        console.log('Very good, continuing');
+        //process.exit();
+    } else {
+        console.log('No can do amigo');
+        throw new Error(
+            'Use `yarn run release`, not `npm publish` or `yarn publish`',
+        );
+    }
+});
